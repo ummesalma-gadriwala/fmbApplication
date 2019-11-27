e5c9594c32b6cc8e5c55fe05770a0e7dc111ad65
hw <hussainweb@gmail.com>
2019-11-27 15:07:17 -0500

    Rename interface to match code standards.

--- client/src/util/FormValidator.ts
+++ client/src/util/FormValidator.ts
@@ -1,6 +1,6 @@
 import validator from 'validator';
 
-export interface IValidationRule {
+interface ValidationRule {
   field: string;
   [method: string]: string | any;
   args?: Array<any>;
@@ -11,9 +11,9 @@ export interface IValidationRule {
 class FormValidator {
   localValidator: any = validator;
 
-  validations: Array<IValidationRule> = [];
+  validations: Array<ValidationRule> = [];
 
-  constructor(validations: Array<IValidationRule>) {
+  constructor(validations: Array<ValidationRule>) {
     // validations is an array of validation rules specific to a form
     this.validations = validations;
   }
@@ -27,16 +27,16 @@ class FormValidator {
       if (!validation[rule.field].isInvalid) {
         // determine the field value, the method to invoke and optional args from
         // the rule definition
-        const field_value = state[rule.field].toString();
+        const fieldValue = state[rule.field].toString();
         const args = rule.args || [];
-        const validation_method = this.localValidator[rule.method];
+        const validationMethod = this.localValidator[rule.method];
 
         // call the validation_method with the current field value as the first
         // argument, any additional arguments, and the whole state as a final
         // argument.  If the result doesn't match the rule.validWhen property,
         // then modify the validation object for the field and set the isValid
         // field to false
-        if (validation_method(field_value, ...args, state) !== rule.validWhen) {
+        if (validationMethod(fieldValue, ...args, state) !== rule.validWhen) {
           validation[rule.field] = { isInvalid: true, message: rule.message };
           validation.isValid = false;
         }

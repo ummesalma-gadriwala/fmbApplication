c27e224a6edd692ac5583738818db9db3549f460
Nuru B <nuruddin.bawangonwala@gmail.com>
2020-09-11 06:31:20 -0400

    Update Salwaat List

--- client/src/components/Dashboard/Dashboard.tsx
+++ client/src/components/Dashboard/Dashboard.tsx
@@ -220,7 +220,6 @@ class Dashboard extends Component<any, DashBoardState> {
                                           <li>Syedna Hatim Bin Ibrahim (RA)</li>
                                           <li>Syedna Taher Saifuddin (RA)</li>
                                           <li>Syedna Mohammed Burhanuddin (RA)</li>
-                                          <li>Syedi Hasanfeer Saheb</li>
                                         </ul>  
                                       </span>  
                                     </div>
